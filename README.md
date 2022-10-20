4f27b4f7d598cb007cddf9f4366ce0c02f857a8f
tdascoli <R0hrch@ch>
2022-10-20 15:03:52 +0200

    fix

--- README.md
+++ README.md
@@ -1,38 +1,65 @@
 # email-obfuscator
 
-obfuscates emails in html pages. searches for html tags with the `.email` class attribute and then generates a `a` tag with a mailto href:
+deobfuscates obfuscated emails in html pages. searches for html tags with the `.email` class attribute and then generates a `a` tag with a mailto href:
 
 ```html
   <span class="email">email [at] domain [dot] tld</span>
 ```
-will be rendered the follwing:
+will be rendered the following:
 
 ```html
   <a class="email" href="mailto:email@domain.tld">email@domain.tld</a>
 ```
 
+you can also deobfuscate obfuscated emails (Important: no whitespaces in obfuscated email) in text blocks. You need to indicate a css selector, in the example below this would be `.selector`:
+
+```javascript
+    $(document).ready(function() {
+        deobfuscateInText(".selector");
+    });
+    
+    // or without jquery
+    
+    document.addEventListener("DOMContentLoaded", function (event) {
+        deobfuscateInText(".selector");
+    });
+```
+html:
+```html
+    <div class="selector">
+        <b>Lorem ipsum dolor sit amet</b>, consetetur sadipscing elitr, sed diam nonumy eirmod tempor name[at]domain[dot]tld invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo Name.Surname[at]mail[DOT]com dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est test[at]test[dot]ch Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+    </div>
+```
+will be rendered the following:
+
+```html
+    <div class="selector">
+        <b>Lorem ipsum dolor sit amet</b>, consetetur sadipscing elitr, sed diam nonumy eirmod tempor <a class="email" href="mailto:name@domain.tld">name@domain.tld</a> invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo <a class="email" href="mailto:Name.Surname@mail.com">Name.Surname@mail.com</a> dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est <a class="email" href="mailto:test@test.ch">test@test.ch</a> Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+    </div>
+```
+
 ## examples
 
 ```html
-  <span class="email" data-display="Thomas D'Ascoli" data-class-replace="own-email-class">thomas [at] dasco [dot] li</span>
+  <span class="email" data-display="Name Surname" data-class-replace="own-email-class">name [at] domain [dot] tld</span>
   
   <span class="email test-class1 test-class2">test [At] test[Dot]ch</span>
   
-  <span class="email" data-subject="test subject" data-body="test body1">fam [AT]dasco [dot]li</span>
+  <span class="email" data-subject="test subject" data-body="test body1">info [AT]company [dot]de</span>
   
-  <span class="email" data-body="test body2">Thomas.DAscoli[at]gmail[DOT]com</span>
+  <span class="email" data-body="test body2">Name.Surname[at]mail[DOT]com</span>
 ```
 
 generates:
 
 ```html
-  <a class="own-email-class" href="mailto:thomas@dasco.li">Thomas D'Ascoli</a>
+  <a class="own-email-class" href="mailto:name@domain.tld">Name Surname</a>
   
   <a class="email test-class1 test-class2" href="mailto:test@test.ch">test@test.ch</a>
   
-  <a class="email" href="mailto:fam@dasco.li?subject=test subject?body=test body1">fam@dasco.li</a>
+  <a class="email" href="mailto:info@company.de?subject=test subject?body=test body1">info@company.de</a>
   
-  <a class="email" href="mailto:Thomas.DAscoli@gmail.com?body=test body2">Thomas.DAscoli@gmail.com</a>
+  <a class="email" href="mailto:Name.Surname@mail.com?body=test body2">Name.Surname@mail.com</a>
 ```
 
 ## options
--- obfuscator.js
+++ obfuscator.js
@@ -1,79 +1,79 @@
-document.addEventListener("DOMContentLoaded", function (event) {
-    document.querySelectorAll(".email").forEach((element, key) => {
-        deobfuscate(element);
+    document.addEventListener("DOMContentLoaded", function (event) {
+        document.querySelectorAll(".email").forEach((element, key) => {
+            deobfuscate(element);
+        });
     });
-});
 
-function deobfuscateInText(selector) {
-    document.querySelectorAll(selector).forEach((block, key) => {
-        const regex = /([^\s]+)\w+\[at\]([^\s]+)\w+\[dot\]+\w([^\s]+)/ig;
-        let content = block.innerHTML;
-        if (content!==undefined) {
-            content.match(regex).forEach((element, key) => {
-                let html = generate(element.replace(/\[at\]/ig, '@').replace(/\[dot\]/ig, '.'), undefined, "email");
-                content = content.replace(element, html.outerHTML);
-            });
-            block.innerHTML = content;
-        }
-    });
-}
-
-function deobfuscate(element) {
-    let config = element.dataset;
-    let css = element.classList;
-    let content = element.textContent.replace(/\s+/g, '');
-    let html = generate(content.replace(/\[at\]/ig, '@').replace(/\[dot\]/ig, '.'), config, css);
-    element.replaceWith(html);
-}
-
-function generate(value, config, css) {
-    let href = 'mailto:' + value;
-    let display = value;
-    const template = document.createElement('a');
-
-    // css
-    if (css !== undefined){
-        template.classList = css;
+    function deobfuscateInText(selector) {
+        document.querySelectorAll(selector).forEach((block, key) => {
+            const regex = /([^\s]+)\w+\[at\]([^\s]+)\w+\[dot\]+[a-zA-Z]+/ig;
+            let content = block.innerHTML;
+            if (content!==undefined) {
+                content.match(regex).forEach((element, key) => {
+                    let html = generate(element.replace(/\[at\]/ig, '@').replace(/\[dot\]/ig, '.'), undefined, "email");
+                    content = content.replace(element, html.outerHTML);
+                });
+                block.innerHTML = content;
+            }
+        });
     }
-    // config
-    if (config!==undefined){
-        if (config.subject) {
-            href = href + '?subject=' + config.subject;
-        }
-        // Body
-        if (config.body) {
-            let sep = '?';
-            if (href.charAt(href.length - 1) === '?') {
-                sep = '&';
-            }
-            href = href + sep + 'body=' + config.body;
-        }
-        // CC
-        if (config.cc) {
-            let sep = '?';
-            if (href.charAt(href.length - 1) === '?') {
-                sep = '&';
-            }
-            href = href + sep + 'cc=' + config.cc;
-        }
-        // BCC
-        if (config.bcc) {
-            let sep = '?';
-            if (href.charAt(href.length - 1) === '?') {
-                sep = '&';
-            }
-            href = href + sep + 'bcc=' + config.bcc;
-        }
-        // Display
-        if (config.display) {
-            display = config.display;
-        }
+
+    function deobfuscate(element) {
+        let config = element.dataset;
+        let css = element.classList;
+        let content = element.textContent.replace(/\s+/g, '');
+        let html = generate(content.replace(/\[at\]/ig, '@').replace(/\[dot\]/ig, '.'), config, css);
+        element.replaceWith(html);
+    }
+
+    function generate(value, config, css) {
+        let href = 'mailto:' + value;
+        let display = value;
+        const template = document.createElement('a');
+
         // css
-        if (config.classReplace) {
-            template.classList = config.classReplace;
+        if (css !== undefined){
+            template.classList = css;
         }
+        // config
+        if (config!==undefined){
+            if (config.subject) {
+                href = href + '?subject=' + config.subject;
+            }
+            // Body
+            if (config.body) {
+                let sep = '?';
+                if (href.charAt(href.length - 1) === '?') {
+                    sep = '&';
+                }
+                href = href + sep + 'body=' + config.body;
+            }
+            // CC
+            if (config.cc) {
+                let sep = '?';
+                if (href.charAt(href.length - 1) === '?') {
+                    sep = '&';
+                }
+                href = href + sep + 'cc=' + config.cc;
+            }
+            // BCC
+            if (config.bcc) {
+                let sep = '?';
+                if (href.charAt(href.length - 1) === '?') {
+                    sep = '&';
+                }
+                href = href + sep + 'bcc=' + config.bcc;
+            }
+            // Display
+            if (config.display) {
+                display = config.display;
+            }
+            // css
+            if (config.classReplace) {
+                template.classList = config.classReplace;
+            }
+        }
+        template.textContent = display;
+        template.setAttribute('href', href);
+        return template;
     }
-    template.textContent = display;
-    template.setAttribute('href', href);
-    return template;
-}
